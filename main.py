c533b98ac18d47c982bb1be67fdb398e43d8a0a3
Sahil <sahilratnaparkhi1@gmail.com>
2025-07-17 16:10:01 +0530

    Send email report with url

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def create_connection(query):
         host = host,
         user = user,
         password  = password,
-        database = database
+        database = database,
     )
 
     cursor = db.cursor()
@@ -49,12 +49,6 @@ def create_connection(query):
     cursor.close()
     db.close()
     return df
-
-# def convert_to_html(query):
-#         html_table = create_connection(query).to_html(index=False)
-#         file_name = f""
-#         with open('slowest_running_queries.html', 'w') as f:
-#             f.write(html_table)
 
 
 def send_mail(html):
@@ -91,27 +85,61 @@ def send_mail(html):
         print(f"Unexpected error: {e}")
         return False
 
+def get_query_url(query_ids, base_url="http://localhost:8080/openspecimen/#/queries/"):
+    query_urls = {}
+    if not query_ids:
+        return query_urls
+    
+    for query_id in query_ids:
+        if query_id is not None:
+            query_url = f"{base_url}addedit?queryId={int(query_id)}"
+            query_urls[query_id] = {
+                'query_id':query_id,
+                'url':query_url
+            }
+            print(f"Constructed url for query_id = {query_id} and {query_url}")
+    return query_urls
+                   
+
+def add_url_to_dataframe(df):
+    query_ids = df['query_id'].dropna().tolist()
+    urls = get_query_url(query_ids)
+    
+    df['url'] = df['query_id'].apply(lambda qid: urls[qid]['url'] if qid in urls else None)
+
+    print("This is the df with url")
+    print(df)
+
+    return df
 
 
 def main():
-    top_5_slowest_running_queries = 'select concat(first_name,\' \',last_name) as user, time_to_finish as time_taken,time_of_exec as Date from catissue_query_audit_logs logs join catissue_user usr on logs.run_by=usr.identifier order by time_to_finish desc limit 5;'
-    top_5_most_run_saved_queries = 'select concat(first_name,\' \',last_name) as name,run_by, count(*) as cnt, Max(time_of_exec) as Date, max(time_to_finish) as time_taken  from catissue_query_audit_logs logs join catissue_user user on logs.run_by = user.identifier where query_id is not null group by name,run_by order by cnt desc limit 5;'
-    top_5_users_running_most_queries = 'select concat(first_name,\' \',last_name) as name, run_by, count(*) as cnt, Max(time_of_exec) as Date, max(time_to_finish) as time_taken from catissue_query_audit_logs logs join catissue_user user on user.identifier = logs.run_by group by run_by limit 5;'
+    top_5_slowest_running_queries = 'select logs.query_id, concat(first_name,\' \',last_name) as user, time_to_finish as time_taken,time_of_exec as Date from catissue_query_audit_logs logs join catissue_user usr on logs.run_by=usr.identifier order by time_to_finish desc limit 5;'
+    top_5_most_run_saved_queries = 'select logs.query_id,concat(first_name,\' \',last_name) as name, count(*) as cnt, Max(time_of_exec) as Date, max(time_to_finish) as time_taken  from catissue_query_audit_logs logs join catissue_user user on logs.run_by = user.identifier where query_id is not null group by name,logs.query_id order by cnt desc limit 5;'
+    top_5_users_running_most_queries = 'select concat(first_name,\' \',last_name) as name, count(*) as cnt, Max(time_of_exec) as Date, max(time_to_finish) as time_taken from catissue_query_audit_logs logs join catissue_user user on user.identifier = logs.run_by group by run_by limit 5;'
     
     print('============Top 5 slow running queries================')
-    slowest_running_queries_html_table = create_connection(top_5_slowest_running_queries).to_html(index=False)
+    slow_queries_df=create_connection(top_5_slowest_running_queries)
+    top_5_slowest_running_queries = add_url_to_dataframe(slow_queries_df)
+    slowest_running_queries_html_table = top_5_slowest_running_queries.to_html(index=False)
     with open('slowest_running_queries.html', 'w') as f:
         f.write(slowest_running_queries_html_table)
 
     
     print('===========Top 5 most run saved queries=================')
-    most_run_saved_queries_html_table = create_connection(top_5_most_run_saved_queries).to_html(index=False)
+    most_run_queries_df = create_connection(top_5_most_run_saved_queries)
+    top_5_most_run_saved_queries =  add_url_to_dataframe(most_run_queries_df)
+    top_5_most_run_saved_queries = top_5_most_run_saved_queries.drop('cnt',axis=1)
+    # print(top_5_most_run_saved_queries.columns)
+    most_run_saved_queries_html_table = top_5_most_run_saved_queries.to_html(index=False)
     with open('most_run_saved_queries.html',mode='w') as f:
         f.write(most_run_saved_queries_html_table)
 
 
     print('===========Top 5 users running most queries=================')
-    users_running_most_queries_html_table  = create_connection(top_5_users_running_most_queries).to_html(index=False)
+    users_running_most_queries=create_connection(top_5_users_running_most_queries)
+    users_running_most_queries=users_running_most_queries.drop('cnt',axis=1)
+    users_running_most_queries_html_table  = users_running_most_queries.to_html(index=False)
     with open('users_running_most_queries.html', mode='w') as f:
         f.write(users_running_most_queries_html_table)
 
@@ -129,11 +157,20 @@ def main():
         <br>
         <h3>Top 5 users running most queries</h3>
         {users_running_most_queries_html_table}
-        <p>Regards,<br>Your Automation Script</p>
+        <div style="margin-top: 30px; padding: 10px; background-color: #f8f9fa; border-radius: 5px;">
+            <p><strong>Regards,</strong><br>Your Database Monitoring Script</p>
+            <p><small>Report generated automatically on {current_datetime}</small></p>
+        </div>
+
+        
     </body>
     </html>
     """
     send_mail(combined_html)
+
+
+    # add_url_to_dataframe(most_run_queries_df)
+    # add_url_to_dataframe(slow_queries_df)
 
 if __name__ == '__main__':
     main()
